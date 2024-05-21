062891f54019e5dcc3532adb542ce45969a1a6aa
Anva41103 <146993960+Anva41103@users.noreply.github.com>
2024-05-22 03:02:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the closest number/find-the-closest-number.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest( int n, int target,int A[]) 
+    { 
+        // Complete the function
+         int start = 0;
+        int end = n - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        // Now check which of A[start] or A[end] is closer to target
+        int leftDiff = abs(A[start] - target);
+        int rightDiff = abs(A[end] - target);
+        
+        if (leftDiff < rightDiff) {
+            return A[start];
+        } else if (rightDiff < leftDiff) {
+            return A[end];
+        } else {
+            // If both differences are the same, return the greater value
+            return max(A[start], A[end]);
+    }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(n,k,arr)<<endl;
+    }
+}
+
+// } Driver Code Ends
